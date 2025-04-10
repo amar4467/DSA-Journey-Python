fa9216aa772e10351f9463f1f7141e7b1c290fcc
amar4467 <amareshsivakumar20@gmail.com>
2025-04-11 00:33:42 +0530

    Added Day 1-Two Sum

---
+++ Day_01/Two_sum.py
@@ -0,0 +1,8 @@
+
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
